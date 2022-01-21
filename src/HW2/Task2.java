ddb0644d28bdffb44271196da95c3d5dd9131b10
Holotova <svetlanagolotova86@gmail.com>
2022-01-21 21:53:17 +0200

    init

---
+++ src/HW2/Task2.java
@@ -0,0 +1,18 @@
+package HW2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println(getArea(2, 2, 3, 5, 6, 1));
+    }
+
+    public static double getArea(double x1, double y1, double x2, double y2, double x3, double y3) {
+
+        double s = 0.5 * (((x1 - x3) * (y2 - y3)) - ((x2 - x3) * (y1 - y3)));
+        return Math.abs(s);
+
+    }
+
+}
+
+
+
---
+++ src/HW2/Task2EvenOdd.java
@@ -0,0 +1,13 @@
+package HW2;
+
+public class Task2EvenOdd {
+    public static void main(String[] args) {
+        System.out.println(checkEven(4));
+    }
+
+    public static boolean checkEven(int a) {
+
+        return (a % 2 == 0);
+
+    }
+}
