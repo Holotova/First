4c9d98b41d314d6ec5171889313172f7baec0773
Holotova <svetlanagolotova86@gmail.com>
2022-01-21 21:54:16 +0200

    init

--- src/HW2/Task2.java
+++ src/HW2/Task2.java
@@ -7,6 +7,7 @@ public class Task2 {
 
     public static double getArea(double x1, double y1, double x2, double y2, double x3, double y3) {
 
+
         double s = 0.5 * (((x1 - x3) * (y2 - y3)) - ((x2 - x3) * (y1 - y3)));
         return Math.abs(s);
 
