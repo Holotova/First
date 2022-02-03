bd98037a47cab0ca48fc33977c42cd18b7e72854
Holotova <svetlanagolotova86@gmail.com>
2022-02-03 22:26:46 +0200

    final changes

--- src/com/ua/holotova/hw4/Task2.java
+++ src/com/ua/holotova/hw4/Task2.java
@@ -5,29 +5,31 @@ import java.util.Arrays;
 
 public class Task2 {
     public static void main(String[] args) {
-        int[] firstArray = new int[10];
+        int[] firstArray = new int[1000];
         fillArray(firstArray);
     }
 
     public static void fillArray(int[] firstArray) {
         int[] secondArray = Arrays.copyOf(firstArray, firstArray.length);
         for (int i = 0; i < secondArray.length; i++) {
-            secondArray[i] = 2 + (int) (Math.random() * 10);
+            secondArray[i] = 1 + (int) (Math.random() * 10);
         }
         System.out.println(Arrays.toString(secondArray));
         int primeNumbers = 0;
         int compositeNumbers = 0;
         for (int i = 0; i < secondArray.length; i++) {
-            boolean isPrime = true;
-            for (int j = 2; j < secondArray[i]; j++) {
-                if (secondArray[i] % j == 0) {
-                    isPrime = false;
+            if (secondArray[i] != 1) {
+                boolean isPrime = true;
+                for (int j = 2; j < secondArray[i]; j++) {
+                    if (secondArray[i] % j == 0) {
+                        isPrime = false;
+                    }
                 }
-            }
-            if (isPrime) {
-                primeNumbers++;
-            } else {
-                compositeNumbers++;
+                if (isPrime) {
+                    primeNumbers++;
+                } else {
+                    compositeNumbers++;
+                }
             }
         }
         System.out.println("Простых чисел " + primeNumbers);
