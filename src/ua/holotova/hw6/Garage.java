c0dd6b34535f8e4c95b17883d3c9e91a418d0bba
Svetlana <97988536+Holotova@users.noreply.github.com>
2022-02-10 21:59:54 +0200

    Revert "hw7"

--- src/ua/holotova/hw6/Garage.java
+++
@@ -1,8 +0,0 @@
-package ua.holotova.hw6;
-
-public class Garage {
-    public static void checkСar(Car o){
-        String a = o.petrol > 50? o.engine >200? "Проверка пройдена": "Недостаточная мощность двигателя!": o.engine >200? "Недостаточно топлива!":"Недостаточно топлива и низкая мощность двигателя!";
-        System.out.println(a);
-    }
-}
