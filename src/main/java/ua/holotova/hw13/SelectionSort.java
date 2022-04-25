cd68c5722fa54b801b611b7ce81447cf726a3243
Holotova <svetlanagolotova86@gmail.com>
2022-04-25 21:38:14 +0300

    for article

--- src/main/java/ua/holotova/hw13/SelectionSort.java
+++ src/main/java/ua/holotova/hw13/SelectionSort.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int [] array = {8, 15, 7, 4, 8, 12, 0, 9, 15, 20, 3};
+        int[] array = {8, 15, 7, 4, 8, 12, 0, 9, 15, 20, 3};
         selectionSort(array);
     }
-    public static void selectionSort (int [] array){
+
+    public static void selectionSort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             int position = i;
             int min = array[i];
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < min){
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
                     position = j;
                     min = array[j];
                 }
